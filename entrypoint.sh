841a707016c189c5c07b4b1e32c1236d9ad0a982
Jakob Ackermann <das7pad@outlook.com>
2020-02-19 12:06:28 +0100

    [misc] narrow down write access/ownership for the run-time user

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,8 @@ DOCKER_GROUP=$(stat -c '%g' /var/run/docker.sock)
 groupadd --non-unique --gid ${DOCKER_GROUP} dockeronhost
 usermod -aG dockeronhost node
 
-mkdir -p /app/cache
-chown -R node:node /app/cache
-
-mkdir -p /app/compiles
-chown -R node:node /app/compiles
-
-chown -R node:node /app/bin/synctex
 mkdir -p /app/test/acceptance/fixtures/tmp/
-chown -R node:node /app
-
-chown -R node:node /app/bin
+chown -R node:node /app/test/acceptance/fixtures
 
 ./bin/install_texlive_gce.sh
 exec runuser -u node -- "$@"
